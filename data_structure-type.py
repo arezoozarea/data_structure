e328b9c682cc149b37b0820d1bcd39cd507a0e1b
a.zare <arezoo.zare9604@gmail.com>
2022-08-21 16:57:41 +0430

    put rellated code in it's repository

--- data_structure-type.py
+++ data_structure-type.py
@@ -2,18 +2,27 @@
 # keys = ['a', 'b', 'c']
 # value = [{"university": "harward", "entery_date": 2005}]
 # input_data = "experiences.educations", [{"university": "harward", "entery_date": 2005}]
+# mydict = {}
+# keys_input = ["experiences", "educations"]
+# value_input = [{"university": "harward", "entery_date": 2005}]
 mydict = {}
-keys_input=["experiences","educations"]
-value_input = [{"university": "harward", "entery_date": 2005}]
-def recur(cur_dict, keys_input):
+
+def recur(keys_input, cur_dict, last_value):
     if len(keys_input) <= 1:
-        cur_dict[keys_input.pop(0)] = value_input
+        cur_dict[keys_input.pop(0)] = last_value
         return cur_dict
     key = keys_input.pop(0)
     cur_dict[key] = {}
-    recur(cur_dict[key], keys_input)
+    recur(keys_input, cur_dict[key], last_value)
     return cur_dict
 
 
-print(recur(mydict, keys_input))
-# structure_type("experiences.educations", [{"university": "harward", "entery_date": 2005}])
+def structure_type(paths, last_value):
+    key_inputs = paths.split(".")
+    return recur(key_inputs, mydict, last_value)
+
+
+structure_type("experiences.educations", [{"university": "harward", "entery_date": 2005}])
+structure_type("experiences.educations", [{"university": "oxford", "entery_date": 2011}])
+
+print(mydict)
