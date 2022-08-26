c57ba1072246da4ae2ee51ab3f59fe5a4d2f4f69
arezoozarea <arezoo.zare9604@gmail.com>
2022-08-26 17:56:41 +0430

    last_edit

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,20 +1,26 @@
 import os
-import re
 
 import pandas
 
 base_path = r"C:\personal_files"
 input_data = os.path.join(base_path, "employee_dataset.txt")
 read_data = pandas.read_csv(input_data, delimiter="\t")
+
+
+def get_column(employee_attribute):
+    employee_result = df[employee_attribute]
+    return employee_result
+
+
 df = pandas.DataFrame(read_data)
-employee_salary = df["Employee_Salary"]
-print (employee_salary.mean())
-print (employee_salary.max())
-print (employee_salary.min())
+print (get_column("Employee_Salary").mean())
+print (get_column("Employee_Salary").max())
+print (get_column("Employee_Salary").min())
 print("colmun_num:" , len(df.columns))
 print("colmun_name:", df.columns)
 print (df["Company_Name"].unique())
-print(len(df.loc[df['Company_Name']=='Nichols-James']))
+print(df.loc[:,["Name"]])
+print(df.loc[df['Name']=='Julie Morton'])
 # --------------
 # print(df[["Employee_Job_Title","Company_Name"]].values)
 # print(pd.unique(df[["Company_Name","Employee_Salary"]].values.ravel()))
@@ -22,9 +28,5 @@ print(len(df.loc[df['Company_Name']=='Nichols-James']))
 # print(pd.concat([df["Company_Name"],df["Employee_Salary"]]))
 # print (len(df["Company_Name"].unique()))
 
-def get_column(employee_attribute):
-    employee_result = df[employee_attribute]
-    return employee_result
 
-print(get_column('Company_Name'))
-
+# print(get_column('Company_Name'))
