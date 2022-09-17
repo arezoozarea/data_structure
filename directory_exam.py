adbf10aaffad3110c77bbd7e38eca82da207bd5a
arezoozarea <arezoo.zare9604@gmail.com>
2022-09-17 19:45:20 +0430

    last_edit

--- directory_exam.py
+++ directory_exam.py
@@ -2,14 +2,19 @@ import os
 from os import path
 
 input_path = r"C:\Users\arezoo\Desktop\file system exam"
-dir_name = r"C:\Users\arezoo\Desktop\out"
+out_path = r"C:\Users\arezoo\Desktop\out"
+zip_file = r"C:\Users\arezoo\Desktop\out\text.zip"
+move_path = r"C:\Users\arezoo\Desktop\out\data"
+if not path.exists(out_path):
+    os.makedirs(out_path)
+if not path.exists(move_path):
+    os.makedirs(move_path)
 
 
 def get_path(input_path):
     file_list = []
     dirs = os.listdir(input_path)
-    if not path.exists(dir_name):
-        os.makedirs(dir_name)
+
     for dir in dirs:
         full_path = path.join(input_path, dir)
         if not "New Text Document" in full_path:
@@ -20,7 +25,21 @@ def get_path(input_path):
 
 
 for item in get_path(input_path):
-    with open(path.join(dir_name, "all_files.txt"), 'a') as f:
-        f.write(item + "\n")
 
-# print(get_path(input_path))
+    file_size = path.getsize(item)
+    if file_size < 100 or file_size != 0:
+        read_file = open(item, "r")
+        a = read_file.readlines()
+        for i in a:
+            if i=="\n":
+                i.replace("\n","")
+            print (i)
+
+
+# for item in get_path(input_path):
+#     with open(path.join(out_path, "all_files.txt"), 'a') as f:
+#         f.write(item + "\n")
+#
+# with ZipFile(zip_file, "w") as zip:
+#     zip.write(path.join(out_path, "all_files.txt"))
+#
